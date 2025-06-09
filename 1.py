9e6ce55684a1a6053c9237ee1141bc9c0305fdce
Harry Jiang <hjiang55@jh.edu>
2025-06-09 18:26:34 -0400

    upload file

--- 1.py
+++ 1.py
@@ -71,5 +71,6 @@ def update_anndata_with_cell_meta(cell_meta, adata_path, index_col='cell_id', ov
 
 
 if __name__ == "__main__":
-    update_anndata_with_cell_meta("/dcl01/hongkai/data/data/hjiang/Data/cell_data.csv", "/dcl01/hongkai/data/data/hjiang/Data/test_rna.h5ad", index_col='barcode')
-    main("/dcl01/hongkai/data/data/hjiang/Data/test_rna.h5ad")
+    # update_anndata_with_cell_meta("/dcl01/hongkai/data/data/hjiang/Data/cell_data.csv", "/dcl01/hongkai/data/data/hjiang/Data/test_rna.h5ad", index_col='barcode')
+    # main("/dcl01/hongkai/data/data/hjiang/Data/test_rna.h5ad")
+    main("/Users/harry/Desktop/GenoDistance/Data/count_data.h5ad")
